c355e0270c1ffbe0384e4f3f27b66cac83a5d83c
Fernando Avalos <fernando@localplantsource.com>
2017-02-08 09:22:57 -0600

    Add default source resource endpoints
    Include default routes for our scrapping endpoints

---
+++ routes/resources/sources.php
@@ -0,0 +1,25 @@
+<?php
+
+// List all resources available in scraper
+Route::get('', [
+  'as' => 'sources.index',
+  'uses' => 'SourceController@index'
+]);
+
+// Display source informaiton
+Route::get('{sourceName}', [
+  'as' => 'sources.show',
+  'uses' => 'SourceController@show'
+]);
+
+// Scrape a source
+Route::get('{sourceName}/scrape', [
+  'as' => 'sources.scrape',
+  'uses' => 'SourceController@scrape'
+]);
+
+// Scrape all sources
+Route::get('/scrape', [
+  'as' => 'sources.all.scrape',
+  'uses' => 'SourceController@ascrape'
+]);
