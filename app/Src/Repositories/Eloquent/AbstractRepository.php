998804b24dfda9cfbde32e383f26c20c8b4077f1
Fernando Avalos <fernando@localplantsource.com>
2017-02-08 14:21:31 -0600

    Include abstract eloquent repository

---
+++ app/Src/Repositories/Eloquent/AbstractRepository.php
@@ -0,0 +1,70 @@
+<?php 
+
+namespace App\Src\Repositories\Eloquent;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Container\Container as App;
+
+
+abstract class AbstractRepository {
+
+    /**
+     * @var Illuminate\Container\Container
+     */
+    private $app;
+
+    /**
+     * @var
+     */
+    protected $model;
+
+    /**
+     * 
+     */
+    public function __construct(App $app) {
+        $this->app = $app;
+        $this->makeModel();
+    }
+
+    /**
+     * Specify Model class name
+     * 
+     * @return mixed
+     */
+    abstract function model();
+
+    /**
+     * @return Model
+     * @throws RepositoryException
+     */
+    public function makeModel() {
+        $model = $this->app->make($this->model());
+
+        if (!$model instanceof Model)
+            throw new RepositoryException("Class {$this->model()} must be an instance of Illuminate\\Database\\Eloquent\\Model");
+
+        return $this->model = $model;
+    }
+
+    /**
+     * 
+     * @param type $data 
+     * @return type
+     */
+    public function create($data) {
+        return $this->model->create($data);
+    }
+
+    /**
+     * 
+     * @param type $id 
+     * @param type $data 
+     * @return type
+     */
+    public function update($id, $data) {
+        $record =  $this->model->find($id);
+        $record->fill($data);
+        $record->save();
+        return $record;
+    }
+}
