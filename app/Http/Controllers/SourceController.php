ad56188f4a14e874252d278eec5963696ea48c80
Fernando Avalos <fernando@localplantsource.com>
2017-02-08 09:22:00 -0600

    Create stub resource controller
    Stub controller for source resource

---
+++ app/Http/Controllers/SourceController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SourceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Scrape by source name
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function scrape($sourceName)
+    {
+        //
+    }
+
+    /**
+     * Scrape all sources
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function ascrape()
+    {
+        //
+    }
+}
