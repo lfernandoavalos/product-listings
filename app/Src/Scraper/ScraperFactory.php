de02cf23cc6a37789f8adc5f643d6a94a0e05f76
Fernando Avalos <fernando@localplantsource.com>
2017-02-08 13:41:30 -0600

    Set tag for source scraper instance

--- app/Src/Scraper/ScraperFactory.php
+++ app/Src/Scraper/ScraperFactory.php
@@ -9,11 +9,20 @@ class ScraperFactory {
 	 * @param string $sourceName
 	 */
 	static public function create($name, $endpoint) {
-		$name = ucwords(strtolower($name));
+		$words = explode('_', $name);
+		$name = '';
+		foreach ($words as $w) {
+			$name .= ucwords(strtolower($w));
+		}		
+
 		$class = "\App\Src\Scraper\\".$name.'Scraper';
+
 		if ( !class_exists($class) ) {
 			throw new \Exception("$class not found", 1);
 		}
-		return new $class($endpoint);
+
+		$instance = new $class($endpoint);
+		$instance->setTag($endpoint);
+		return $instance;
 	}
 }
