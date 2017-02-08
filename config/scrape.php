1364935c48bd5b3cb4cb094958a62ab82e0ae32d
Fernando Avalos <fernando@localplantsource.com>
2017-02-08 12:29:22 -0600

    Update scrape config include MercadoLibre settings

--- config/scrape.php
+++ config/scrape.php
@@ -4,5 +4,9 @@ return [
 	'LINIO' => [
 		'url' => 'https://www.linio.com.mx',
 		'products_search_endpoint' => '/search?q='
+	],
+	'MERCADO_LIBRE' => [
+		'url' => 'http://listado.mercadolibre.com.mx',
+		'products_search_endpoint' => '/'
 	]
 ];
