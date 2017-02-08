4f567582b9613aa87dfbf99cf086073b9324cf67
Fernando Avalos <fernando@localplantsource.com>
2017-02-08 12:29:38 -0600

    Include MercadoLibre product links scraper

---
+++ app/Src/Scraper/MercadoLibreScraper.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Src\Scraper;
+
+use DOMDocument;
+use DomXPath;
+
+// 
+class MercadoLibreScraper extends AbstractScraper
+{
+
+    const SOURCE_NAME = 'MERCADO_LIBRE';
+
+    /**
+     * Perform scraper setup
+     */
+    protected function setup() {
+        $this->config = config('scrape.'.self::SOURCE_NAME);
+    }
+
+    /**
+     * Build search endpoint url
+     * 
+     * @param string $productName
+     * @return string
+     */
+    protected function constructSearchEndpoint() {
+        $this->url = implode('', [
+            $this->config['url'],
+            $this->config['products_search_endpoint'],
+            $this->endpoint
+        ]);
+
+    }
+
+
+
+    /**
+     * Run scraper to fetch products
+     * 
+     * @param string $productName
+     * @return type
+     */
+    public function scrape() {
+
+        $pnodes = $this->getNodes($this->dom, 'item-link');
+        
+        foreach ($pnodes as $count => $a) {
+            
+            if ($count > $this->limit) {
+                break;
+            }
+            $href = $a->getAttribute('href');
+        }
+    }
+}
