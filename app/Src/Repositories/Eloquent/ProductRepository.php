7cbd06e0e35ef722943498f7ebecffce6e2b6215
Fernando Avalos <fernando@localplantsource.com>
2017-02-08 14:22:00 -0600

    Include eloquent product repository

---
+++ app/Src/Repositories/Eloquent/ProductRepository.php
@@ -0,0 +1,61 @@
+<?php 
+
+namespace App\Src\Repositories\Eloquent;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Container\Container as App;
+use App\Src\ValueObject\ProductValueObject;
+
+class ProductRepository extends AbstractRepository {
+ 
+    /**
+     * Specify Model class name
+     *
+     * @return Model
+     */
+    function model()
+    {
+        return 'App\Src\Models\Product';
+    }
+
+    /**
+     * Description
+     * @param type $sourceUrl 
+     * @return type
+     */
+    public function fetchByUrl($sourceUrl) {
+        return $this->model->where('source_url', $sourceUrl)->first();
+    }
+
+    /**
+     * Store batch or single product
+     * @param mixed $mixed 
+     * @return type
+     */
+    public function store(ProductValueObject $product) {
+        $sourceUrl = $product->getSourceUrl();
+        $record = $this->fetchByUrl($sourceUrl);
+
+        if(!$record) {
+            $data = $product->toArray();
+            unset($data['tag']);
+            $tag = $product->getTag();
+            $data['tags'] = json_encode([$tag => $tag]);
+            $record = $this->create($data);
+        } else {
+            $data = $product->toArray();
+            unset($data['tag']);
+            $tag = $product->getTag();
+                
+            $tags = $record->tags;
+            if(!isset($tags->$tag)) {
+                $tags->$tag = $tag;
+            }
+
+            $data['tags'] = json_encode($tags);
+            $record = $this->update($record->id, $data);
+        }
+
+        return $record;
+    }
+}
