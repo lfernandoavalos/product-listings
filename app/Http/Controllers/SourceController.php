252e75869d87f78db69a60cca36c3cfc883a6aae
Fernando Avalos <fernando@localplantsource.com>
2017-02-08 11:24:33 -0600

    Require search product name for source scraper
    Include scraper factory to generate scraper by source name

--- app/Http/Controllers/SourceController.php
+++ app/Http/Controllers/SourceController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Src\Scraper\ScraperFactory;
 
 class SourceController extends Controller
 {
@@ -39,12 +40,39 @@ class SourceController extends Controller
 
     /**
      * Scrape by source name
-     *
+     * 
+     * @param string $sourceName
      * @return \Illuminate\Http\Response
      */
-    public function scrape($sourceName)
+    public function scrape(Request $request, $sourceName)
     {
+        $product = trim($request->get('search'));
+
+        if(!$product) {
+            return response()->json([
+                'success' => false,
+                'message' => "search is a required field"
+            ]);
+        }
+        
         //
+        try{
+            $scraper = ScraperFactory::create($sourceName, $product);
+            $scraper->scrape();
+        }catch(\Exception $e) {
+            var_dump($e->getMessage());
+            // TODO: Setup logging 
+            return response()->json([
+                'success' => false,
+                'message' => "Looks like something went wrong"
+            ]);
+        }
+        
+
+        return response()->json([
+            "success" => true,
+            "message" => "$sourceName was successfully scrapped"
+        ]);
     }
 
     /**
