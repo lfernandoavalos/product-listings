144ca8739d9a65753ee89431e19e8da0e861a4ef
Fernando Avalos <fernando@localplantsource.com>
2017-02-08 11:26:00 -0600

    Linio products scraper - Generate single urls
    Implementation of Linio products scraper generate single linio product
    url

---
+++ app/Src/Scraper/LinioScraper.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Src\Scraper;
+
+use DOMDocument;
+use DomXPath;
+
+// 
+class LinioScraper extends AbstractScraper
+{
+
+    const SOURCE_NAME = 'LINIO';
+
+    /**
+     * Perform scraper setup
+     */
+    protected function setup() {
+        $this->config = config('scrape.'.self::SOURCE_NAME);
+    }
+
+    /**
+     * Build search endpoint url
+     * 
+     * @param string $productName
+     * @return string
+     */
+    protected function constructSearchEndpoint() {
+        $this->url = implode('', [
+            $this->config['url'],
+            $this->config['products_search_endpoint'],
+            $this->endpoint
+        ]);
+
+    }
+
+    /**
+     * Run scraper to fetch products
+     * 
+     * @param string $productName
+     * @return type
+     */
+    public function scrape() {
+        $pnodes = $this->getNodes($this->dom, 'detail-container');
+        foreach ($pnodes as $productContainerNode) {
+            $productNode = $this->getNodes($productContainerNode, 'title-section');
+            foreach ($productNode as $node) {
+                $tags = $this->getTags($node, 'a');
+                $a = $tags->item(0);
+                $href = $a->getAttribute('href');
+            }
+        }
+    }
+}
