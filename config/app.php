e3cc70eaf91d4b34f5404fe55dd90fa1b49240c6
Fernando Avalos <fernando@localplantsource.com>
2017-02-08 16:50:04 -0600

    Update app config with jwt provider

--- config/app.php
+++ config/app.php
@@ -168,7 +168,7 @@ return [
          */
 
         //
-
+        Tymon\JWTAuth\Providers\JWTAuthServiceProvider::class,
         /*
          * Application Service Providers...
          */
@@ -226,6 +226,8 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
 
+        'JWTAuth' => Tymon\JWTAuth\Facades\JWTAuth::class,
+        'JWTFactory' => Tymon\JWTAuth\Facades\JWTFactory::class
     ],
 
 ];
