c090ca80c61abfddba22d3a464f62f500b2a10d5
Fernando Avalos <fernando@localplantsource.com>
2017-02-08 11:25:02 -0600

    Abstract scraper class

---
+++ app/Src/Scraper/AbstractScraper.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace App\Src\Scraper;
+
+use DOMDocument;
+use DomXPath;
+use DOMNodeList;
+use DOMElement;
+
+// 
+abstract class AbstractScraper
+{
+    /**
+     * @var array $config
+     */
+    protected $config;
+
+    /**
+     * @var string $url
+     */
+    protected $url;
+
+    /**
+     * @var \DOMDocument
+     */
+    protected $dom;
+
+    /**
+     * @var string
+     */
+    protected $productName;
+
+    /**
+     * @var array
+     */
+    protected $products;
+
+    /**
+     * Constructor
+     * @return 
+     */
+    public function __construct($endpoint = null) {
+        //
+        $this->endpoint = $endpoint;
+        // 
+        $this->dom = new DOMDocument();
+        // construct base url
+        $this->setup();
+        // construct search endpoint
+        $this->constructSearchEndpoint();
+        // Load html
+        $this->setDomDocumentUrl();
+    }
+
+    /**
+     * Description
+     * @param DOMDocument $dom 
+     * @param string $tag 
+     * @return type
+     */
+    protected function getTags($dom, $tag) {
+        if ( !($dom instanceof DOMDocument) ) {
+            $html = $dom->ownerDocument->saveHTML($dom);
+            $dom = new DOMDocument;
+            $dom->loadHTML($html);    
+        }
+        
+        return $dom->getElementsByTagName($tag);
+    }
+
+    /**
+     * Retrieve nodes by classname
+     * 
+     * @param \DOMDocument $dom
+     * @param string $className 
+     * @return DOMNodeList
+     */
+    protected function getNodes($dom, $classname) {
+        if ( $dom instanceof DOMElement) {
+            $html = $dom->ownerDocument->saveHTML($dom);
+            $dom = new DOMDocument;
+            $dom->loadHTML($html);
+        }
+
+        $finder = new DomXPath($dom);
+        return $finder->query("//*[contains(concat(' ', normalize-space(@class), ' '), ' $classname ')]");
+    }
+
+    /**
+     * 
+     * @return type
+     */
+    protected function setDomDocumentUrl() {
+        @$this->dom->loadHTMLFile($this->url);
+    }
+
+    /**
+     * Let children perform setup
+     */
+    abstract protected function setup();
+
+    /**
+     * Product name to scrape
+     * @return array
+     */
+    abstract public function scrape();
+
+    /**
+     * Create search endpoint
+     * @return
+     */
+    abstract protected function constructSearchEndpoint();
+}
+
